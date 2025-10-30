3514e6f4697cb3e9f100447a5defd64b97bdb7e6
Dheeraj Reddy <dheerajchinna456@gmail.com>
2025-10-31 00:03:12 +0530

    fix: remove custom global-error directory - not needed in Next.js 15

--- app/_global-error/page.tsx
+++
@@ -1,8 +0,0 @@
-import GlobalErrorClient from "./global-error-client"
-
-export default function GlobalErrorPage(props: {
-  error: Error & { digest?: string }
-  reset: () => void
-}) {
-  return <GlobalErrorClient {...props} />
-}
